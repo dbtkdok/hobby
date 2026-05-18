9437511a08f15ead6575551e638c02ecf114003a
dbtkdok <57377065+dbtkdok@users.noreply.github.com>
2026-05-18 14:58:59 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -4,9 +4,15 @@
  */
 var cnt = 0;
 var cntList = new Array();
-var final_1 = [1,4,7,10,13,16,19,22,25,28,31,34];
-var final_2 = [2,5,8,11,14,17,20,23,26,29,32,35];
-var final_3 = [3,6,9,12,15,18,21,24,27,30,33,36];
+var final_1 = [1,2,3];
+var final_2 = [4,5,6];
+var final_3 = [7,8,9];
+var final_4 = [10,11,12];
+var final_5 = [13,14,15];
+var final_6 = [16,17,18];
+var final_7 = [19,20,21];
+var final_8 = [22,23,24];
+var final_9 = [25,26,27];
 
 $(document).ready(function () {
     //HTML 과 CSS 의 모든 로딩이 끝나면 J-Query 를 실행.
@@ -83,6 +89,12 @@ function fnMovePage(id, nextID) {
             var final_cnt_1 = 0;
             var final_cnt_2 = 0;
             var final_cnt_3 = 0;
+            var final_cnt_4 = 0;
+            var final_cnt_5 = 0;
+            var final_cnt_6 = 0;
+            var final_cnt_7 = 0;
+            var final_cnt_8 = 0;
+            var final_cnt_9 = 0;
             var sum_final_score = 0;
             cntList.map(function(element){
                 final_1.map(function(element1){
@@ -105,23 +117,78 @@ function fnMovePage(id, nextID) {
                         final_cnt_3 += element.get("step_" + element3);
                     }
                 })
+
+                final_4.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_3 += element.get("step_" + element3);
+                    }
+                })
+
+                final_5.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_4 += element.get("step_" + element3);
+                    }
+                })
+
+                final_6.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_5 += element.get("step_" + element3);
+                    }
+                })
+
+                final_7.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_6 += element.get("step_" + element3);
+                    }
+                })
+
+                final_8.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_7 += element.get("step_" + element3);
+                    }
+                })
+
+                final_9.map(function(element3){
+                    if(element.get("step_" + element3) != undefined && element.get("step_" + element3) != null) {
+                        //console.log(element.get("step_" + element3));
+                        final_cnt_8 += element.get("step_" + element3);
+                    }
+                })
             })
-            var finalin = Math.max(final_cnt_1, final_cnt_2, final_cnt_3);
+            var finalin = Math.max(final_cnt_1, final_cnt_2, final_cnt_3, final_cnt_4, final_cnt_5, final_cnt_6, final_cnt_7, final_cnt_8, final_cnt_9);
             $(id).addClass("none");
             $("#section_final").removeClass("none", "");
-            /*
+           
             if(final_cnt_1 == finalin) {
-                $("#section_final").css('background-image', 'url(./img/final_1.png)');
+                $("#section_final").css('background-image', 'url(./img/final_result_1.png)');
             } else if(final_cnt_2 == finalin) {
-                $("#section_final").css('background-image', 'url(./img/final_2.png)');
+                $("#section_final").css('background-image', 'url(./img/final_result_2.png)');
             } else if(final_cnt_3 == finalin) {
-                $("#section_final").css('background-image', 'url(./img/final_3.png)');
-            } */
+                $("#section_final").css('background-image', 'url(./img/final_result_3.png)');
+            } else if(final_cnt_4 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_4.png)');
+            } else if(final_cnt_5 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_5.png)');
+            } else if(final_cnt_6 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_6.png)');
+            } else if(final_cnt_7 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_7.png)');
+            } else if(final_cnt_8 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_8.png)');
+            } else if(final_cnt_9 == finalin) {
+                $("#section_final").css('background-image', 'url(./img/final_result_9.png)');
+            } 
+             
             sum_final_score = final_cnt_1 + final_cnt_2 + final_cnt_3;
             //$("#fin_1").text(final_cnt_1);
             //$("#fin_2").text(final_cnt_2);
             //$("#fin_3").text(final_cnt_3);
-            $("#fin_top").text("90");
+            //$("#fin_top").text("90점");
             //console.log("final_cnt_1 :: ", final_cnt_1);
             //console.log("final_cnt_2 :: ", final_cnt_2);
             //console.log("final_cnt_3 :: ", final_cnt_3);
